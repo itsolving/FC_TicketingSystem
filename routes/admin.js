b5aff258a2d58db2bdaa945d2a018bedd750900c
Kuanysh Tuktubayev <kuanysh.tuktubayev@mail.ru>
2018-11-29 21:58:11 +0600

    продолжение разработки страницы редактирования пользователей в админке

--- routes/admin.js
+++ routes/admin.js
@@ -562,6 +562,31 @@ router.get('/user/:id', function(req, res, next) {
 			}
 		}
 		client.end();
+					const clientRoles = new Client(conOptions);
+					clientRoles.connect();
+					var sSQLRoles = 'SELECT r."ID", r."Name" from public."tRole" r ';
+					console.log(sSQLRoles);
+					clientRoles.query(sSQLRoles, (qerrRoles, qresRoles) => {
+						if (qerrRoles) {
+							console.log(qerrRoles ? qerrRoles.stack : qresRoles);
+						}
+						else {
+							console.log(qerrRoles ? qerrRoles.stack : qresRoles);
+							
+							if (typeof qresRoles.rowCount === 'undefined') {
+								console.log('res.rowCount not found');
+							}
+							else {
+								if (qresRoles.rowCount == 0) {
+									console.log('res.rowCount='+qresRoles.rowCount);
+								}
+								else {
+									rolesList = qresRoles.rows;
+								}
+							}
+						}
+						clientRoles.end();
+					});
 		res.render('adminuseredit', {title: 'Админка', adminLogin: sAdminLogin, userData: rowUserData, userID: nID, roles: rolesList});
 	});
 });
