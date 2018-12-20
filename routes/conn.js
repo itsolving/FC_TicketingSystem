d9b15b588626f437afcdeb03631a128f5b64eb86
Ivan <shanti.sniper@gmail.com>
2018-12-20 18:29:03 +0300

    gulp и фронтенд

--- routes/conn.js
+++ routes/conn.js
@@ -3,11 +3,11 @@
 //для админки использую "свой" коннект к БД. Потому что запарился с общим коннектом из файла "queries.js"
 const { Client } = require('pg');
 const conOptions = {
-	//user: 'postgres', //local test on home-computer
-	user: 'pgadmin', //test on dev-server
+	user: 'postgres', //local test on home-computer
+	//user: 'pgadmin', //test on dev-server
 	
-	//password: 'qwe', //local test on home-computer
-	password: 'UrdodON9zu83BvtI6L', //test on dev-server
+	password: 'asd', //local test on home-computer
+  //password: 'qwe', //test on dev-server
 	
 	host: 'localhost',
 	database: 'postgres',
