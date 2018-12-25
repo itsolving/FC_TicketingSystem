fb76c61504f765577b7c041bd792b8163fc8c49a
Ivan <shanti.sniper@gmail.com>
2018-12-25 17:04:56 +0300

    фронт схемы без расположения билетов (требуется дизайн)

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -9,15 +9,38 @@ app.init = function () {
   app.zoomTribune(zoom);
 	app.getEventsList();
 	app.checkEvent();
-  app.getTickets(1);
-  app.endPreloading();
+  app.getTickets(1); // 1 - временный id при загрузке страницы.
+  // На проде передавать id динамически при выборе соответствующего мероприятия.
+};
+
+app.calibrateTribune = function(tribune){
+  var leter1 = tribune[0],
+      leter2 = tribune[1],
+      cal = {
+        calibrateX: 0,
+        calibrateY: 0
+      };
+
+  switch(leter1){
+    case 'W': cal.calibrateX = 30; cal.calibrateY = -50; break;
+    case 'S': cal.calibrateX = -50;
+              cal.calibrateY = (leter2 === 'E') ? 150 : (leter2 === 'W') ? 0 : 50; break;
+    case 'E': cal.calibrateX = 30; cal.calibrateY = 150; break;
+    case 'N': cal.calibrateX = 100;
+              cal.calibrateY = (leter2 === 'E') ? 150 : (leter2 === 'W') ? 0 : 50; break;
+  }
+
+  return cal;
 };
 
 app.zoomTribune = function(zoom){
   $('[data-tribune]').each(function(){
     var _self = $(this);
-    _self.on('click', function(e){
-      console.log(e);
+    _self.on('click', function(){
+      var scaleMultiplier = 2,
+          selfData = app.calibrateTribune(_self.data('tribune'));
+
+      zoom.zoomAbs(_self.offset().left + selfData.calibrateX, _self.offset().top + selfData.calibrateY, scaleMultiplier);
     });
   });
 };
@@ -42,8 +65,15 @@ app.getTickets = function(id){
   $.post('/gettickets', {IDEvent: id}, function(data){
     if(data.ReqStatus === "success") {
       console.log(data);
+      data.TicketData.forEach(function(tribune){
+        tribune.row_to_json.tickets.forEach(function(ticket){
+          //console.log(ticket.RowN);
+          //$('[data-seat]').clone().appendTo($('[data-tribune-line]').filter('[data-tribune-line="' + ticket.RowN + '"]'));
+        });
+      });
     }
   });
+  app.endPreloading();
 };
 
 app.endPreloading = function(){
