7c30ec2bb17ea948c090af6d6539d4726e86c3b0
Kuanysh Tuktubayev <kuanysh.tuktubayev@mail.ru>
2019-01-02 13:08:08 +0600

    from Develop to Master (2019-01-02 13:09 gmt+6)

--- routes/conn.js
+++ routes/conn.js
@@ -5,14 +5,14 @@ const { Client } = require('pg');
 const conOptions = {
 	user: 'pgadmin', //version for server
 	//user: 'postgres', //local test on home-computer
-	
+
 	password: 'UrdodON9zu83BvtI6L', //version for server
 	//password: 'qwe', //local test on home-computer Kuanysh
 	//password: 'asd', //local test on home-computer Ivan
-	
+
 	host: 'localhost', //version for server
 	//host: '185.22.64.26', //local test on home by connect to dev-server
-	
+
 	database: 'postgres',
 	port: 5432,
 };
@@ -23,4 +23,3 @@ module.exports = {
 	connClient: Client,
 	conOptions: conOptions
 }
-
