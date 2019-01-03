2c770c6747a22740163966f11857506c30dbda9c
Kuanysh Tuktubayev <33995581+KuanyshTuktubayev@users.noreply.github.com>
2019-01-03 08:50:44 +0600

    public files
    загрузка файлов в папке public

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -7,11 +7,10 @@ $(function () {
 app.init = function () {
   var zoom = panzoom(document.getElementById('stadiumSVG'), {smoothScroll: false});
   app.zoomTribune(zoom);
-	app.getEventsList();
-	app.checkEvent();
-  app.getTickets(1); // 1 - временный id при загрузке страницы.
-  // На проде передавать id динамически при выборе соответствующего мероприятия.
-  app.initPopup();
+	app.getEventsList(); //Temp
+	app.checkEvent(); //Temp
+  app.getTickets(1);
+  app.endPreloading();
 };
 
 app.calibrateTribune = function(tribune){
@@ -63,27 +62,24 @@ app.checkEvent = function(){
 };
 
 app.getTickets = function(id){
-  $.post('/gettickets', {IDEvent: id}, function(data){
-    if(data.ReqStatus === "success") {
-      console.log(data);
-      data.TicketData.forEach(function(tribune){
-        tribune.row_to_json.tickets.forEach(function(ticket){
-          //console.log(ticket.RowN);
-          //$('[data-seat]').clone().appendTo($('[data-tribune-line]').filter('[data-tribune-line="' + ticket.RowN + '"]'));
-        });
+  var $tribune = $('[data-tribune]');
+  id = id !== 'undefined' ? id : 1;
+  $tribune.each(function(){
+    var _self = $(this);
+    _self.on('click', function(){
+      $.post('/getsectortickets', {IDEvent: id, SectorName: _self.data('tribune')}, function(data){
+        if(data.TicketData.length > 0){
+          var $secInfo = data.TicketData[0].row_to_json;
+          console.log($secInfo);
+          $.fancybox.open({
+            src: '/public/images/sectors/' + _self.data('tribune') + '.svg',
+            type: 'ajax'
+          });
+        }else{
+          console.log('Билетов нет!');
+        }
       });
-    }
-  });
-  app.endPreloading();
-};
-
-app.initPopup = function(){
-  var $btn = $('[data-init-popup]'),
-      $popup = $('[data-popup]');
-
-  $btn.on('click', function(){
-    console.log('lol');
-    $.fancybox.open($popup);
+    });
   });
 };
 
