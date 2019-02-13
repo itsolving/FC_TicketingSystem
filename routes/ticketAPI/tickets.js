f5de1dc2de9b6d858bfdf15bda978a2339b56166
nextwell <just.nextwell@gmail.com>
2019-02-13 21:51:19 +0300

    новый метод post для апи белетов, проход

--- routes/ticketAPI/tickets.js
+++ routes/ticketAPI/tickets.js
@@ -50,4 +50,31 @@ module.exports = (router, dbUtils) => {
 			else res.json({err: "билет не найден"})
 		})
 	})
+
+	router.post('/passage/put/', function(req, res){
+		let reqData = {
+			IDEvent: req.body.idevent, 
+			Barcode: req.body.barcode,
+			type:    req.body.type
+		}
+		dbUtils.Ticket.getByBarcode(reqData, (ticket) => {
+			if ( ticket ){
+				dbUtils.Ticket.getPassage({ID: ticket.ID, Barcode: ticket.Barcode, IDEvent: ticket.IDEvent}, (result) => {
+					if ( result.ID ){
+						// проход уже был зафиксирован
+						res.json({err: "проход уже зафиксирован"})
+					}
+					else {
+						dbUtils.Ticket.insertPassage({IDTicket: ticket.ID, IDEvent: ticket.IDEvent, IDUserController: reqData.type}, (data) => {
+							if (data.ID){
+								res.json({ok: "Проход успешен!"});
+							}
+							else res.json({err: "Ошибка прохода!"});
+						})
+					}
+				})
+			}
+			else res.json({err: "билет не найден"})
+		})
+	})
 }
