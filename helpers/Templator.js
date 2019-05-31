64f0565967e26932b24b33a48b35a10203144e3e
nextwell <just.nextwell@gmail.com>
2019-05-31 06:04:28 +0300

    шаблоны

--- helpers/Templator.js
+++ helpers/Templator.js
@@ -55,8 +55,8 @@ class Templator{
 							scale: 3, 
 							height: 10, 
 							includetext: true, 
-							textxalign: 'center',
-							rotate: 'R'
+							textxalign: 'center'
+							// rotate: 'R'
 						};
 					    let reqAPI = "http://bwipjs-api.metafloor.com/?"
 					    for(var attr in options) { 
