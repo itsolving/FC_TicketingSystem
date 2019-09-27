15ca06f254a8595975f82548ce23695655ab26de
nextwell <just.nextwell@gmail.com>
2019-09-27 23:22:08 +0300

    fix почтового отправления

--- helpers/mailer.js
+++ helpers/mailer.js
@@ -4,6 +4,13 @@ let nodemailer = require('nodemailer'),
 
 class eMailVerification{
 	constructor(){
+	//	this.smtp = nodemailer.createTransport({		 		
+ 	// 	    service: "Gmail",		 			
+ 	// 	    auth: {		 		   
+ 	// 	        user: "ticketscloud.astana",		 		   
+ 	// 	        pass: "f5a175acac4a00aa702424d53b01903a"		 		    
+ 	// 	    }
+ 	// 	});
 		this.smtp = nodemailer.createTransport({
 			service: 'Yandex',
 		    auth: {
@@ -35,16 +42,11 @@ class eMailVerification{
             }
         }
 
-        let host = "92.46.109.122:8109";
-        // let host = data.req.get('host');
-        // if ( data.req.get('host') != 'localhost'){
-        // 	host = host + ":8109";
-        // }
 	    let link = `http://${host}/kassa/get/main/tickets/A4/${hash}/${tickids}`;
 
 	    mailOptions.attachments.push({
 	    	filename: 'tickets.pdf',
-        	path: link
+        	path: `http://localhost:3000/kassa/get/main/tickets/A4/${hash}/${tickids}`
 	    })
 
 	    mailOptions.html = mailOptions.html + `<a href="${link}">Билеты</a><br>`;
@@ -64,21 +66,14 @@ class eMailVerification{
 	    }
 
 	    let host = "92.46.109.122:8109";
-        // let host = data.req.get('host');
-        // if ( data.req.get('host') != 'localhost'){
-        // 	host = host + ":8109";
-        // }
-	 
 
 	    tickets.forEach((ticket, index) => {
 	    	let hash = md5((ticket.ID + ticket.IDEvent + ticket.Barcode));
-	    	console.log(ticket);
-	    	console.log((ticket.ID + ticket.IDEvent + ticket.Barcode) + " | " + hash);
 	    	let link =`http://${host}/cloud/ticket/A4/${ticket.ID}/${hash}`;
 	    	mailOptions.html = mailOptions.html + `<a href="${link}">Билет №${index+1}</a><br>`;
 	    	mailOptions.attachments.push({
 		    	filename: `Ticket_${index+1}.pdf`,
-	        	path: link
+	        	path: `http://localhost:3000/cloud/ticket/A4/${ticket.ID}/${hash}`
 		    })
 	    })
 	    this.smtp.sendMail(mailOptions, (error, response) => {
@@ -93,55 +88,6 @@ class eMailVerification{
                     next(true); 
                 })	
 			}
-	     })
-	}
-	sendFromGmail(data, tickets, next){
-		// let smtp = nodemailer.createTransport({		 		
- 	// 	    service: "Gmail",		 			
- 	// 	    auth: {		 		   
- 	// 	        user: "ticketscloud.astana",		 		   
- 	// 	        pass: "f5a175acac4a00aa702424d53b01903a"		 		    
- 	// 	    }
- 	// 	});
-		console.log(tickets);
-		let mailOptions={
-	        to : data.mail,
-	        from: 'tickets@fcastana.kz',
-	        subject : `${tickets[0].Name} | Билеты на мероприятие`,
-	        html : `Здравствуйте.<br> Ваши купленные билеты доступны по следующей ссылке:<br><br>`,
-	        attachments: []
-	    }
-
-	    let hash = md5((tickets[0].ID + tickets[0].IDEvent + tickets[0].Barcode))
-
-	    let tickids = '';
-
-	    for ( var i = 0; i < tickets.length; i++){
-            if ( i == tickets.length-1 ){
-            	tickids += `${tickets[i].ID}`;
-          	} 
-         	else {
-           		tickids += `${tickets[i].ID},`;
-            }
-        }
-
-        let host = "92.46.109.122:8109";
-        // let host = data.req.get('host');
-        // if ( data.req.get('host') != 'localhost'){
-        // 	host = host + ":8109";
-        // }
-	    let link = `http://${host}/kassa/get/main/tickets/A4/${hash}/${tickids}`;
-
-	    mailOptions.attachments.push({
-	    	filename: 'tickets.pdf',
-        	path: `http://localhost:3000/kassa/get/main/tickets/A4/${hash}/${tickids}`
-	    })
-
-	    mailOptions.html = mailOptions.html + `<a href="${link}">Билеты</a><br>`;
-
-	    this.smtp.sendMail(mailOptions, (error, response) => {
-		     if(error) console.log(error)
-		     else next() 
 	     })
 	}
 
